5b92fa464d8807a0a65eb20ef8e27da018070519
Carlos Prado <carlosesteban0204@gmail.com>
2023-02-13 13:01:20 -0500

    state incompleted

--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -34,7 +34,7 @@ function TodoProvider(props) {
     const completeTodo = (text) => {
         const todoIndex = todos.findIndex(todo => todo.text === text);
         const newTodos = [...todos];
-        (newTodos[todoIndex].completed === false)
+        (!newTodos[todoIndex].completed)
             ? newTodos[todoIndex].completed = true
             : newTodos[todoIndex].completed = false
         saveTodos(newTodos);
